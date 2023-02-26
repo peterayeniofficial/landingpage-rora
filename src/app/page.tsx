60478c36159648ff7b6f66663aad0d51b2e410b1
Peter Ayeni <ayenicology@gmail.com>
2023-02-26 22:11:41 +0000

    add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,67 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Rora:&nbsp;
-          <code className={styles.code}>Stay Alert, Stay Safe</code>
-        </p>
-        {/* <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div> */}
+    <main
+    // className="bg-cover bg-center h-screen"
+    // style={{ backgroundImage: "url('/rora-bg.png')" }}
+    >
+      <nav className="fixed flex justify-between py-6 w-full lg:px-48 md:px-12 px-4 content-center bg-secondary z-10">
+        <div className="flex items-center">
+          <Image src="/Rora.svg" alt="Logo" width={123} height={59} />
+        </div>
+
+        <div className="font-montserrat hidden md:block">
+          <button className="py-2 px-4 text-white bg-black rounded-3xl">
+            Join the wait list
+          </button>
+        </div>
+      </nav>
+      <div className="relative h-screen">
+        <Image
+          className="object-cover w-full h-full"
+          src="/rora-bg.png"
+          alt="Rora"
+          width={1280}
+          height={832}
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black bg-opacity-60">
+          <section className="pt-24 md:mt-0 md:h-screen flex flex-col justify-center text-center md:text-left md:flex-row md:justify-between md:items-center lg:px-48 md:px-12 px-4 bg-secondary">
+            <div className="md:flex-1 md:mr-10">
+              <h1 className="font-pt-serif text-5xl font-bold mb-7 text-white">
+                Stay
+                <span className="bg-underline1 bg-left-bottom bg-no-repeat pb-2 bg-100% font-inter font-bold text-4xl leading-14 text-yellow-400">
+                  Alert
+                </span>
+                , Stay
+                <span className="bg-underline1 bg-left-bottom bg-no-repeat pb-2 bg-100% font-inter font-bold text-4xl leading-14 text-yellow-400">
+                  Safe
+                </span>
+              </h1>
+              <p className="font-pt-serif font-normal mb-7 text-white text-xl">
+                Empowering people with the tools they need to protect themselves
+                and their loved ones — whether they’re walking home from work or
+                commuting to school.
+              </p>
+              <div className="font-montserrat">
+                <p className="text-gray-400 text-lg pb-3">
+                  Know when we are live!
+                </p>
+                <button className="bg-red-600 px-6 py-4 rounded-lg border-2 border-red-600 border-solid text-white mr-2 mb-2">
+                  Join the wait list
+                </button>
+                <p className="text-white text-xs">
+                  By sharing your email, you agree to our Subscriber Agreement &
+                  Privacy Policy
+                </p>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
 
-      {/* <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div> */}
+      {/* <div classNameName="absolute inset-0 bg-gradient-to-t from-black to-transparent bg-opacity-75 z-0"></div> */}
     </main>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      "pt-serif": ["PT Serif", "serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+    },
+    backgroundSize: {
+      auto: "auto",
+      cover: "cover",
+      contain: "contain",
+      "100%": "100%",
+    },
+    extend: {
+      backgroundImage: {
+        underline1: "url('/Underline1.svg')",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+      },
+    },
+  },
+  plugins: [],
+};
